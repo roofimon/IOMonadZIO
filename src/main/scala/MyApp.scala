f0960aab206c7116f994e5eb41af87754695c8ee
RuuF <roof@odds.team>
2024-11-24 13:02:39 +0700

    Remove unuse logic

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -2,12 +2,6 @@ import zio._
 import zio.Console._
 
 object MyApp extends ZIOAppDefault {
-  // def run = fileOperations
-
-  // def fileOperations: ZIO[Console, Throwable, Unit] = for {
-  //   lines <- FileOperations.readLines("example.txt")
-  //   _ <- Console.printLine(lines.mkString("\n"))
-  // } yield ()
   def run = myAppLogic
 
   val myAppLogic: ZIO[Any, Throwable, Unit] =
